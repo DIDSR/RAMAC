78a55907bcb0beb5b0e351835aff0744e3c528d2
qiancao <qiancao227@gmail.com>
2024-02-26 14:27:04 -0500

    promoted Subrata's branch to ramac

--- ramac/registration.py
+++ ramac/registration.py
@@ -1,110 +1,172 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Feb 24 12:39:23 2024
+#!/usr/bin/env python
+# coding: utf-8
 
-Tools for image registration.
+# In[5]:
 
-@author: Qian.Cao
-"""
 
+import os
+import pydicom
 import numpy as np
+from skimage import measure
+from scipy import ndimage
 import SimpleITK as sitk
+import pandas as pd
+from skimage import draw
+from typing import Any, Dict, List, Optional, Tuple, TypeVar, Union
+from pydicom.errors import InvalidDicomError
+import pickle
+import csv
+import pandas as pd
+import pickle
+import ast
+import re
+get_ipython().run_line_magic('matplotlib', 'inline')
+import matplotlib.pyplot as plt
+from scipy.spatial.distance import cdist
+from scipy.optimize import linear_sum_assignment
+from pydicom.errors import InvalidDicomError
 
-def _setup_rigid_registration():
-    """
+from ipywidgets import interact, fixed
+from IPython.display import clear_output
 
-    Creates a simple registration object with metric, optimizer and interpolator
 
-    Returns
-    -------
-    R : sitk.ImageRegistrationMethod
-        Parameters for Rigid Registration
+# In[6]:
 
-    """
+
+import os
+import csv
+
+
+
+# Callback invoked by the interact IPython method for scrolling through the image stacks of
+# the two images (moving and fixed).
+def display_images(fixed_image_z, moving_image_z, fixed_npa, moving_npa):
+    # Create a figure with two subplots and the specified size.
+    plt.subplots(1,2,figsize=(10,8))
     
-    R = sitk.ImageRegistrationMethod()
-    R.SetMetricAsCorrelation()
-    R.SetOptimizerAsRegularStepGradientDescent(
-        learningRate=2.0,
-        minStep=1e-4,
-        numberOfIterations=500,
-        gradientMagnitudeTolerance=1e-8,
+    # Draw the fixed image in the first subplot.
+    plt.subplot(1,2,1)
+    plt.imshow(fixed_npa[fixed_image_z,:,:],cmap=plt.cm.Greys_r);
+    plt.title('fixed image')
+    plt.axis('off')
+    
+    # Draw the moving image in the second subplot.
+    plt.subplot(1,2,2)
+    plt.imshow(moving_npa[moving_image_z,:,:],cmap=plt.cm.Greys_r);
+    plt.title('moving image')
+    plt.axis('off')
+    
+    plt.show()
+    
+def display_images_with_alpha(image_z, alpha, fixed, moving):
+    img = (1.0 - alpha)*fixed[:,:,image_z] + alpha*moving[:,:,image_z] 
+    plt.imshow(sitk.GetArrayViewFromImage(img),cmap=plt.cm.Greys_r);
+    plt.axis('off')
+    plt.show()
+    
+# Callback invoked when the StartEvent happens, sets up our new data.
+def start_plot():
+    global metric_values, multires_iterations
+    
+    metric_values = []
+    multires_iterations = []
+
+# Callback invoked when the EndEvent happens, do cleanup of data and figure.
+def end_plot():
+    global metric_values, multires_iterations
+    
+    del metric_values
+    del multires_iterations
+    # Close figure, we don't want to get a duplicate of the plot latter on.
+    plt.close()
+
+# Callback invoked when the IterationEvent happens, update our data and display new figure.
+def plot_values(registration_method):
+    global metric_values, multires_iterations
+    
+    metric_values.append(registration_method.GetMetricValue())                                       
+    # Clear the output area (wait=True, to reduce flickering), and plot current data
+    clear_output(wait=True)
+    # Plot the similarity metric values
+    plt.plot(metric_values, 'r')
+    plt.plot(multires_iterations, [metric_values[index] for index in multires_iterations], 'b*')
+    plt.xlabel('Iteration Number',fontsize=12)
+    plt.ylabel('Metric Value',fontsize=12)
+    plt.show()
+    
+# Callback invoked when the sitkMultiResolutionIterationEvent happens, update the index into the 
+# metric_values list. 
+def update_multires_iterations():
+    global metric_values, multires_iterations
+    multires_iterations.append(len(metric_values))
+
+
+# In[17]:
+
+
+def registration_3d_rigid_series (fixed_image, moving_image):
+    initial_transform = sitk.CenteredTransformInitializer(fixed_image, 
+                                                      moving_image, 
+                                                      sitk.Euler3DTransform(), 
+                                                      sitk.CenteredTransformInitializerFilter.GEOMETRY)
+    registration_method = sitk.ImageRegistrationMethod()
+
+    # Similarity metric settings.
+    registration_method.SetMetricAsMattesMutualInformation(numberOfHistogramBins=50)
+    registration_method.SetMetricSamplingStrategy(registration_method.RANDOM)
+    registration_method.SetMetricSamplingPercentage(0.1)
+
+    registration_method.SetInterpolator(sitk.sitkLinear)
+
+    # Optimizer settings.
+    registration_method.SetOptimizerAsGradientDescent(
+        learningRate=0.1,
+        numberOfIterations=1000,
+        convergenceMinimumValue=1e-10,
+        convergenceWindowSize=10,
     )
-    R.SetOptimizerScalesFromIndexShift()
-    R.SetInterpolator(sitk.sitkLinear)
-    
-    return R
+    registration_method.SetOptimizerScalesFromPhysicalShift()
 
-def register_image(fixed: sitk.Image,
-                   moving: sitk.Image) -> sitk.Transform:
-    """
-    Perform registration of fixed and moving images, returns resulting transformation.
+    # Setup for the multi-resolution framework.
+    registration_method.SetShrinkFactorsPerLevel(shrinkFactors=[2, 1, 1])
+    registration_method.SetSmoothingSigmasPerLevel(smoothingSigmas=[4, 2, 1])
+    registration_method.SmoothingSigmasAreSpecifiedInPhysicalUnitsOn()
 
-    Parameters
-    ----------
-    fixed : sitk.Image
-        DESCRIPTION.
-    moving : sitk.Image
-        DESCRIPTION.
+    # Don't optimize in-place, we would possibly like to run this cell multiple times.
+    registration_method.SetInitialTransform(initial_transform, inPlace=False)
 
-    Returns
-    -------
-    outTx : TYPE
-        DESCRIPTION.
-
-    """
-    
-    # Set up rigid registration parameters
-    R = _setup_rigid_registration()
-    
-    # Set up initial transform
-    tx = sitk.CenteredTransformInitializer(
-        fixed, moving, sitk.Similarity3DTransform()
+    # Connect all of the observers so that we can perform plotting during registration.
+    registration_method.AddCommand(sitk.sitkStartEvent, start_plot)
+    registration_method.AddCommand(sitk.sitkEndEvent, end_plot)
+    registration_method.AddCommand(
+        sitk.sitkMultiResolutionIterationEvent, update_multires_iterations
     )
-    R.SetInitialTransform(tx)
+    registration_method.AddCommand(
+        sitk.sitkIterationEvent, lambda: plot_values(registration_method)
+    )
     
-    # add command
-    # R.AddCommand(sitk.sitkIterationEvent, lambda: command_iteration(R))
+    final_transform = registration_method.Execute(
+    sitk.Cast(fixed_image, sitk.sitkFloat32), sitk.Cast(moving_image, sitk.sitkFloat32)
+    )
     
-    # run registration
-    outTx = R.Execute(fixed, moving)
+    print(f"Final metric value: {registration_method.GetMetricValue()}")
+    print(
+    f"Optimizer's stopping condition, {registration_method.GetOptimizerStopConditionDescription()}"
+    )
     
-    return outTx
+    moving_resampled = sitk.Resample(
+    moving_image,
+    fixed_image,
+    final_transform,
+    sitk.sitkLinear,
+    0.0,
+    moving_image.GetPixelID(),
+    )
+    
+    return moving_resampled, [initial_transform, final_transform]
 
-def transform_image(moving: sitk.Image, fixed: sitk.Image, transform: sitk.Transform) -> sitk.Image:
-    """
-    Transform moving image according to input transform, needs fixed image as the reference image.
+#moving_resampled, [init_transform, final_transform] = registration_3d_rigid_series(fixed_image, moving_image)
 
-    Parameters
-    ----------
-    moving : sitk.Image
-        DESCRIPTION.
-    transform : sitk.Transform
-        DESCRIPTION.
 
-    Returns
-    -------
-    Resampeld moving image: sitk.Image
 
-    """
-    
-    # Create resampler
-    resampler = sitk.ResampleImageFilter()
-    resampler.SetReferenceImage(fixed)
-    resampler.SetInterpolator(sitk.sitkLinear)
-    resampler.SetDefaultPixelValue(0)
-    resampler.SetTransform(transform)
-    
-    # Resampled image
-    out = resampler.Execute(moving)
-    
-    return out
 
-def transform_point(point: tuple, transform: sitk.Transform):
-    """
-    Transform a point in physical coordinates using sitk.Transform, returns transformed point in physical coordinates
-    """
-    
-    transformed_point = sitk.TransformPoint(point)
-    
-    return transformed_point
