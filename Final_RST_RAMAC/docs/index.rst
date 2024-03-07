9e4fc7f4ea2bff980285a55e5f41c4bebd401863
Mukherjee <Subrata.Mukherjee@fda.gov>
2024-03-07 09:28:56 -0500

    Added the docs

---
+++ Final_RST_RAMAC/docs/index.rst
@@ -0,0 +1,26 @@
+.. RAMAC documentation master file, created by
+   sphinx-quickstart on Thu Mar  7 09:11:22 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to RAMAC's documentation!
+=================================
+
+.. include:: ../README.md
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+.. toctree::
+    :maxdepth: 2
+    :caption: API:
+
+   modules
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ Final_RST_RAMAC/ramac/preprocessing.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+import SimpleITK as sitk
+
+def mask_air(image, air_threshold=-950):
+    """
+    Mask out air voxels using air_threhold.
+
+    Parameters
+    ----------
+    image : np.array
+        Image to be preprocessed.
+    air_threshold : float, optional
+        Threshold for air voxels. The default is -950.
+
+    Returns
+    -------
+    masked_image : np.array
+        Processed image with air voxels removed.
+
+    """
+    
+    # Create a binary mask using thresholding
+    binary_mask = image > air_threshold
+    
+    # Perform connected component analysis
+    labeled_mask = sitk.ConnectedComponent(binary_mask)
+    
+    # Compute statistics of connected components
+    label_sizes = sitk.LabelShapeStatisticsImageFilter()
+    label_sizes.Execute(labeled_mask)
+    
+    # Find the label with the largest size (excluding label 0, which corresponds to background)
+    largest_label = max(range(1, label_sizes.GetNumberOfLabels() + 1), key=lambda label: label_sizes.GetPhysicalSize(label))
+    
+    # Create a binary mask containing only the largest connected component
+    largest_air_mask = labeled_mask == largest_label
+    
+    # Mask the original image with the largest air mask
+    masked_image = sitk.Mask(image, largest_air_mask)
+    
+    return masked_image
